91801b026f3cf987cdbeae5b66b9cb6f11b45d52
timausk <timausk@googlemail.com>
2020-07-12 07:06:50 +0200

    [script] add various browsers linux/packages

---
+++ scripts/linux/packages/browsers.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#
+# install various browser
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# chromium
+sudo snap install chromium
+
+# midori
+sudo snap install midori
